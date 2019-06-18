32b3d97e0e64b67cd5ddedc7a592a056d3b5e803
AndreyCanon <andrey.canon@edunext.co>
2019-06-18 12:34:30 -0500

    Set lastError on the methods set and get values

--- scormxblock/scormxblock.py
+++ scormxblock/scormxblock.py
@@ -28,6 +28,77 @@ from xblock.fragment import Fragment
 _ = lambda text: text
 FILES_THRESHOLD_FOR_ASYNC = 150
 
+WRITE_MODEL_DATA = [
+    'cmi.core.lesson_location',
+    'cmi.core.lesson_status',
+    'cmi.core.score.raw',
+    'cmi.core.score.max',
+    'cmi.core.score.min',
+    'cmi.core.exit',
+    'cmi.core.session_time',
+    'cmi.suspend_data',
+    'cmi.comments',
+    'cmi.objectives.n.id',
+    'cmi.objectives.n.score.raw',
+    'cmi.objectives.n.score.max',
+    'cmi.objectives.n.score.min',
+    'cmi.objectives.n.status',
+    'cmi.student_preference.audio',
+    'cmi.student_preference.language',
+    'cmi.student_preference.speed',
+    'cmi.student_preference.text',
+    'cmi.interactions.n.id',
+    'cmi.interactions.n.objectives.n.id',
+    'cmi.interactions.n.time',
+    'cmi.interactions.n.type'
+    'cmi.interactions.n.correct_responses.n.pattern',
+    'cmi.interactions.n.weighting',
+    'cmi.interactions.n.student_response',
+    'cmi.interactions.n.result',
+    'cmi.interactions.n.latency',
+]
+
+READ_MODEL_DATA = [
+    'cmi.core._children',
+    'cmi.core.student_id',
+    'cmi.core.student_name',
+    'cmi.core.credit',
+    'cmi.core.entry',
+    'cmi.core.score_children',
+    'cmi.core.lesson_status',
+    'cmi.core.score.raw',
+    'cmi.core.lesson_location',
+    'cmi.core.score.max',
+    'cmi.core.score.min',
+    'cmi.core.total_time',
+    'cmi.core.lesson_mode',
+    'cmi.suspend_data',
+    'cmi.launch_data',
+    'cmi.comments',
+    'cmi.comments_from_lms',
+    'cmi.objectives._children',
+    'cmi.objectives._count',
+    'cmi.objectives.n.id',
+    'cmi.objectives.n.score._children',
+    'cmi.objectives.n.score.raw',
+    'cmi.objectives.n.score.max',
+    'cmi.objectives.n.score.min',
+    'cmi.objectives.n.status',
+    'cmi.student_data._children',
+    'cmi.student_data.mastery_score',
+    'cmi.student_data.max_time_allowed',
+    'cmi.student_data.time_limit_action',
+    'cmi.student_preference._children',
+    'cmi.student_preference.audio',
+    'cmi.student_preference.language',
+    'cmi.student_preference.speed',
+    'cmi.student_preference.text',
+    'cmi.interactions._children',
+    'cmi.interactions._count',
+    'cmi.interactions.n.objectives._count',
+    'cmi.interactions.n.correct_responses._count',
+]
+
 
 @task(name=u'scormxblock.scormxblock.s3_upload', routing_key=settings.HIGH_PRIORITY_QUEUE)
 def s3_upload(all_content, temp_directory, dest_dir):
@@ -73,6 +144,22 @@ def updoad_all_content(temp_directory, fs):
     else:
         # The raw number of files is going to make this request time out. Use celery instead
         s3_upload.apply_async((all_content, temp_directory, dest_dir), serializer='pickle')
+
+
+def validate_property(key, valid_keys):
+
+    words_in_key = key.rsplit(".")
+    len_words = len(words_in_key)
+
+    if len_words == 3:
+        return key in valid_keys
+    elif len_words == 5:
+        new_key = "{}.{}.n.{}.{}".format(words_in_key[0], words_in_key[1], words_in_key[3], words_in_key[4])
+        return new_key in valid_keys
+    elif len_words == 6:
+        new_key = "{}.{}.n.{}.n.{}".format(words_in_key[0], words_in_key[1], words_in_key[3], words_in_key[5])
+        return new_key in valid_keys
+    return False
 
 
 class ScormXBlock(XBlock):
@@ -145,7 +232,9 @@ class ScormXBlock(XBlock):
         frag.add_css(self.resource_string("static/css/scormxblock.css"))
         frag.add_javascript(self.resource_string("static/js/src/scormxblock.js"))
         settings = {
-            'version_scorm': "SCORM_12"
+            'version_scorm': "SCORM_12",
+            'valid_write_data': WRITE_MODEL_DATA,
+            'valid_read_data': READ_MODEL_DATA,
         }
         frag.initialize_js('ScormXBlock', json_args=settings)
         return frag
@@ -206,7 +295,17 @@ class ScormXBlock(XBlock):
             'cmi.suspend_data': self.suspend_data,
         }
 
-        values.update(self.data_scorm)
+        data = {}
+
+        for key, value in self.data_scorm.iteritems():
+            if key in READ_MODEL_DATA:
+                data[key] = value
+            elif key.startswith('cmi.interactions.') and validate_property(key, READ_MODEL_DATA):
+                data[key] = value
+            elif key.startswith('cmi.objectives.') and validate_property(key, READ_MODEL_DATA):
+                data[key] = value
+
+        values.update(data)
         return Response(json.dumps(values), content_type='application/json')
 
     @XBlock.json_handler
@@ -238,11 +337,14 @@ class ScormXBlock(XBlock):
             elif name == 'cmi.suspend_data':
                 self.suspend_data = value
 
-            elif name.startswith('cmi.interactions.'):
+            elif name.startswith('cmi.interactions.') and validate_property(name, WRITE_MODEL_DATA):
                 self.data_scorm['cmi.interactions._count'] = self.data_scorm.get('cmi.interactions._count', 0) + 1
                 self.data_scorm[name] = value
 
-            else:
+            elif name.startswith('cmi.objectives.') and validate_property(name, WRITE_MODEL_DATA):
+                self.data_scorm[name] = value
+
+            elif name in WRITE_MODEL_DATA:
                 self.data_scorm[name] = value
 
         context.update({"completion_status": self.get_completion_status()})
--- scormxblock/static/js/src/scormxblock.js
+++ scormxblock/static/js/src/scormxblock.js
@@ -4,11 +4,12 @@ function ScormXBlock(runtime, element, settings) {
 
     this.LMSInitialize = function(){
       console.log('LMSInitialize');
-      return "true";
+      lastError = 0;
+      return 'true';
     };
 
     this.LMSFinish = function() {
-      console.log("LMSFinish");
+      console.log('LMSFinish');
       return StoreValues();
     };
 
@@ -16,34 +17,34 @@ function ScormXBlock(runtime, element, settings) {
     this.LMSSetValue = SetValue;
 
     this.LMSCommit = function() {
-      console.log("LMSCommit")
+      console.log('LMSCommit');
       return StoreValues();
     };
 
     this.LMSGetLastError = function() {
-      console.log("GetLastError");
-      return "0";
+      console.log('GetLastError');
+      return lastError;
     };
 
     this.LMSGetErrorString = function(errorCode) {
-      console.log("LMSGetErrorString");
-      return "Some Error";
+      console.log('LMSGetErrorString');
+      return SCORM_12_ERRORS[errorCode.toString()];
     };
 
     this.LMSGetDiagnostic = function(errorCode) {
-      console.log("LMSGetDiagnostic");
-      return "Some Diagnostice";
+      console.log('LMSGetDiagnostic');
+      return 'Some Diagnostice';
     }
   }
 
   function SCORM_2004_API(){
     this.Initialize = function(){
       console.log('LMSInitialize');
-      return "true";
+      return 'true';
     };
 
     this.Terminate = function() {
-      console.log("LMSFinish");
+      console.log('LMSFinish');
       return StoreValues();
     };
 
@@ -51,75 +52,117 @@ function ScormXBlock(runtime, element, settings) {
     this.SetValue = SetValue;
 
     this.Commit = function() {
-      console.log("LMSCommit")
+      console.log('LMSCommit')
       return StoreValues();
     };
 
     this.GetLastError = function() {
-      console.log("GetLastError");
-      return "0";
+      console.log('GetLastError');
+      return '0';
     };
 
     this.GetErrorString = function(errorCode) {
-      console.log("LMSGetErrorString");
-      return "Some Error";
+      console.log('LMSGetErrorString');
+      return 'Some Error';
     };
 
     this.GetDiagnostic = function(errorCode) {
-      console.log("LMSGetDiagnostic");
-      return "Some Diagnostice";
+      console.log('LMSGetDiagnostic');
+      return 'Some Diagnostice';
     }
   }
 
-  var values = null;
+  var lastError = 301;
+
+  var SCORM_12_ERRORS = {
+    '0': 'No Error',
+    '101': 'General Exception',
+    '201': 'Invalid argument error',
+    '202': 'Element cannot have children',
+    '203': 'Element not an array. Cannot have count',
+    '301': 'Not initialized',
+    '401': 'Not implemented error',
+    '402': 'Invalid set value, element is a keyword',
+    '403': 'Element is read only',
+    '404': 'Element is write only',
+    '405': 'Incorrect Data Type',
+  };
+
+  var values = [];
   var LMSValues = {};
 
   var GetValue = function (cmi_element) {
-    var handlerUrl = runtime.handlerUrl(element, 'scorm_get_values');
 
-    if (values==null) {
-      var response = $.ajax({
-        type: "GET",
-        url: handlerUrl,
-        async: false
-      });
-      values = JSON.parse(response.responseText);
+    if (settings.valid_read_data.includes(cmi_element)){
+      var handlerUrl = runtime.handlerUrl(element, 'scorm_get_values');
+
+      if (values.length===0) {
+        var response = $.ajax({
+          type: 'GET',
+          url: handlerUrl,
+          async: false,
+        });
+        values = JSON.parse(response.responseText);
+      }
+
+      var value = values[cmi_element];
+
+      if (value==undefined){
+        value = '';
+      }
+
+      console.log('Getvalue for ' + cmi_element + ' = ' + value);
+      return value
+    } else if (settings.valid_write_data.includes(cmi_element)) {
+      lastError = 404;
+    } else if (cmi_element.endsWith('_count')){
+      lastError = 203;
+    } else if (cmi_element.endsWith('_children')){
+      lastError = 202;
+    } else {
+      lastError = 401;
     }
+    return '';
 
-    var value = values[cmi_element];
-
-    if (value==undefined){
-      value = '';
-    }
-
-    console.log("Getvalue for " + cmi_element + " = " + value);
-    return value
   };
 
   var SetValue = function (cmi_element, value) {
-    console.log("LMSSetValue " + cmi_element + " = " + value);
-    LMSValues[cmi_element] = value
-    return "true";
+
+    if (value == null || value == 'undefined'){
+      lastError = 405;
+    } else if (settings.valid_write_data.includes(cmi_element)) {
+      console.log('LMSSetValue ' + cmi_element + ' = ' + value);
+      LMSValues[cmi_element] = value
+      return 'true';
+    } else if (cmi_element.endsWith('_count') || cmi_element.endsWith('_children')){
+      lastError = 402;
+    } else if (settings.valid_read_data.includes(cmi_element)){
+      lastError = 403;
+    } else {
+      lastError = 401;
+    }
+    return 'false';
+
   };
 
   var StoreValues = function () {
     var handlerUrl = runtime.handlerUrl( element, 'scorm_set_values');
 
     $.ajax({
-      type: "POST",
+      type: 'POST',
       url: handlerUrl,
       data: JSON.stringify(LMSValues),
       async: false,
       success: function(response){
-        if (typeof response.lesson_score != "undefined"){
-          $(".lesson_score", element).html(response.lesson_score);
+        if (typeof response.lesson_score != 'undefined'){
+          $('.lesson_score', element).html(response.lesson_score);
         }
-        $(".completion_status", element).html(response.completion_status);
+        $('.completion_status', element).html(response.completion_status);
         LMSValues = {};
       }
     });
 
-    return "true";
+    return 'true';
   };
 
   $(function ($) {
@@ -128,6 +171,6 @@ function ScormXBlock(runtime, element, settings) {
     } else {
       API_1484_11 = new SCORM_2004_API();
     }
-    console.log("Initial SCORM data...");
+    console.log('Initial SCORM data...');
   });
 }
