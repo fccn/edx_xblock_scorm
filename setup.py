d22e7e34594d1296982a8b2fabd9cca06345a2fb
David Moreno <david.moreno@edunext.co>
2020-10-28 16:49:23 -0500

    Bump version: 1.1.0 → 1.1.1

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def package_data(pkg, roots):
 
 setup(
     name='scormxblock-xblock',
-    version='1.1.0',
+    version='1.1.1',
     description='scormxblock XBlock',   # TODO: write a better description.
     packages=[
         'scormxblock',
